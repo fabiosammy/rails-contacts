dd36eb848c72889c88c2455e492c6760059af7da
Fabio L. Janiszevski <contato@fabiosammy.com>
2018-08-16 14:22:34 -0300

    Added the validates and enum

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ApplicationRecord
+  enum gender: [:male, :female]
+  validates_presence_of :name, :gender
+  validates_presence_of :salary, if: :employed?
+  validates_uniqueness_of :name
 end
