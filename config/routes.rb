52392d4af290dbd67857dcc292d6cce51687c12e
Fabio L. Janiszevski <contato@fabiosammy.com>
2018-08-23 21:55:03 -0300

    Disable cors

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :contacts do
     resources :phones, controller: 'contact/phones'
   end
+  match '*path', action: 'handle_options_request', via: [:get, :post, :options]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
