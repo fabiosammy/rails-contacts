1cf20905891d505c4b85fe3b0a4c3e85cf76751f
Fabio L. Janiszevski <contato@fabiosammy.com>
2018-08-23 21:51:37 -0300

    Disable cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+
+  before_action :cors_preflight_check
+  after_action :cors_set_access_control_headers
+
+  protected
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      # ...
+    end
+  end
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    # ...
+  end
 end
